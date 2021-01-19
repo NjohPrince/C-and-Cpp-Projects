ff0ee350bbe6c5836422d5e1690ad859f14dac37
NjohPrince <jufredprince@gmail.com>
2021-01-20 00:01:01 +0100

    added the create new node function

--- PolynomialsLinkedList.c
+++ PolynomialsLinkedList.c
@@ -10,8 +10,35 @@ struct Poly_Node {
     struct Poly_Node* next;
 };
 
+// Function to create new node
+void create_poly(int x, int y, struct Poly_Node** P) {
+    struct Poly_Node *r, *z;
+    z = *P;
+    if (z == NULL) {
+        r = (struct Poly_Node*)malloc(sizeof(struct Poly_Node));
+        r->coef = x;
+        r->pow = y;
+        *P = r;
+        r->next = (struct Poly_Node*)malloc(sizeof(struct Poly_Node));
+        r = r->next;
+        r->next = NULL;
+    }
+    else {
+        r->coef = x;
+        r->pow = y;
+        r->next = (struct Poly_Node*)malloc(sizeof(struct Poly_Node));
+        r = r->next;
+        r->next = NULL;
+    }
+}
+
 int main(void) {
 	
+	struct Poly_Node *poly1 = NULL;
+	
+	create_poly(2, 3, &poly1);
+    create_poly(4, 1, &poly1);
+    create_poly(2, 0, &poly1);
 	
     return 0;
 }
