75328cc08257451f7bddab294fc8b0edbfe5948c
NjohPrince <jufredprince@gmail.com>
2021-01-19 23:59:32 +0100

    project structure

---
+++ PolynomialsLinkedList.c
@@ -0,0 +1,17 @@
+// Library files import
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+// Structure for holding the coefficient and power of a node
+struct Poly_Node {
+    int coef;
+    int pow;
+    struct Poly_Node* next;
+};
+
+int main(void) {
+	
+	
+    return 0;
+}
